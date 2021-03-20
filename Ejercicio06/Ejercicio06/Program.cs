60c5111d2da81ec78dfb300e78e726d82ccec042
diego.taub <diego.taub@accenture.com>
2021-03-20 18:26:54 -0300

    Ejercicio06

---
+++ Ejercicio06/Ejercicio06/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo c;
+            do
+            {
+                c = Console.ReadKey();
+            } while (!c.Modifiers.HasFlag(ConsoleModifiers.Control) || !c.Modifiers.HasFlag(ConsoleModifiers.Shift) || c.Key.ToString().ToUpper() != "F");
+        }
+    }
+}
